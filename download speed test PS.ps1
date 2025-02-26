a5c1d95458623c112a6b9cfbe2359306ff50131b
gr3g0ps <81196208+gr3g0ps@users.noreply.github.com>
2025-02-26 14:57:57 +0100

    Add files via upload

---
+++ download speed test PS.ps1
@@ -0,0 +1,13 @@
+﻿$item = get-item '\\Server_fqdn_or_IP\Folder_name\myfile_wg.pdf'
+$dst = "c:\users\$env:USERNAME\desktop\myfile_wogo.pdf"
+$time=Measure-Command -Expression {Copy-Item -literalpath '\\Server_fqdn_or_IP\Folder_name\myfile_wg.pdf' '$dst'} 
+$TransferRate = ($item.length/1024/1024) / $time.TotalSeconds
+write-host "$TransferRate MB/Sec"
+
+$result = New-Object -TypeName psobject -Property @{
+                    Source           = $item.fullname
+                    TimeTaken        = $time.TotalSeconds
+                    TransferRateMBs  = $TransferRate
+                    }
+
+$result
