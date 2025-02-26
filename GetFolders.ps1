e3162f17e8bb76ed11d68e980f2b2f3a6f91aa53
gr3g0ps <81196208+gr3g0ps@users.noreply.github.com>
2025-02-26 14:55:45 +0100

    Add files.

---
+++ GetFolders.ps1
@@ -0,0 +1,20 @@
+﻿$folders = Get-Content -Path $PSScriptRoot\folders.txt
+$servers = Get-Content -Path $PSScriptRoot\servers.txt
+
+$hits = @()
+
+foreach($s in $servers) {
+    Write-Host "Querying" $s -BackgroundColor Cyan -ForegroundColor Black
+    $drives = Invoke-Command -ComputerName $s -ScriptBlock {gdr -PSProvider 'FileSystem'}
+    foreach($d in $drives) {
+        Write-Host "Searching $d..." -ForegroundColor Yellow
+        foreach($f in $folders) {
+            $hits += Get-ChildItem -Recurse -Filter $f -Directory -ErrorAction SilentlyContinue -Path $("\\" + $s + "\" + $d.Name + "$") | select FullName
+        }
+    }
+
+    foreach($h in $hits) {
+    Get-ChildItem $h.FullName | Select Fullname,@{Name="Owner";Expression={ (Get-Acl $_.FullName).Owner }} | Export-Csv -Path $PSScriptRoot\output.csv -Append
+    $hits = @()
+    }
+}
